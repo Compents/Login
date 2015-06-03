e87efef73a9e08d3ae2ed78b36446550c2efe00c
yangyalin <yyllove123@hotmail.com>
2015-06-03 16:13:07 +0800

    add code and login ui finshed

---
+++ YYLogin/YYLoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  YYLoginViewController.swift
+//  YYLoginViewController
+//
+//  Created by Yalin on 15/6/3.
+//  Copyright (c) 2015年 yalin. All rights reserved.
+//
+
+import UIKit
+
+class YYLoginViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logoUpConstraint: NSLayoutConstraint!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonPressed(sender: UIButton) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YYLoginViewController/YYLoginViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YYLoginViewController
+//
+//  Created by Yalin on 15/6/3.
+//  Copyright (c) 2015年 yalin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
